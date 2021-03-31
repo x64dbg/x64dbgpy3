46affd994e9a11cf57303af30a0036ee8432bb26
robertsi <3690720+robertsi@users.noreply.github.com>
2021-03-31 11:13:58 +0200

    redirected python stdout and stderr to x64dbg log

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 WIP: needs a lot of restructuring, this is just a POC
 
-## Building
+## Building plugin
 
 ```
 cmake -Bbuild -DX64DBGPY3_PYTHON_EXECUTABLE="c:/Python39/python.exe" -DX64DBGPY3_X64DBG_INSTALL_PATH="%USERPROFILE%\Programs\x64dbg\snapshot_2021-03-28_17-36"
 ```
 
-Then open `x64dbgpy3.sln` and change to RelWithDebInfo in Visual Studio, otherwise you get error in runtime:
+Then open `x64dbgpy3.sln` 
+
+### Python pkg build
+python -m build
+
+### Python pkg install
 ```
-Error: AttributeError: 'NoneType' object has no attribute 'write'
+python -m pip install --upgrade pip
+python -m pip install --upgrade wheel
+python -m pip install --upgrade setuptools
+python -m pip install --upgrade build
+
+pip install .
 ```
--- src/x64dbgpy3/plugin.cpp
+++ src/x64dbgpy3/plugin.cpp
@@ -1,8 +1,8 @@
 #include "plugin.h"
+
 #include <pybind11/embed.h>
+//#include <pybind11/iostream.h>
 namespace py = pybind11;
-
-
 
 enum menu_entry
 {
@@ -11,12 +11,12 @@ enum menu_entry
 
 void pluginHandleMenuCommand(CBTYPE cbType, PLUG_CB_MENUENTRY * info)
 {
-    //dprintf("pluginHandleMenuCommand hEntry = %d\n", info->hEntry);
+    // dprintf("pluginHandleMenuCommand hEntry = %d\n", info->hEntry);
     switch (info->hEntry)
     {
     case menu_entry::MENU_ABOUT:
     default:
-        MessageBoxA(hwnd_dlg, "Made By ripperuts", PLUGIN_NAME " Plugin", MB_ICONINFORMATION);
+        MessageBoxA(hwnd_dlg, "Made by ripperuts, mrexodia", PLUGIN_NAME " Plugin", MB_ICONINFORMATION);
         break;
     }
 }
@@ -24,39 +24,56 @@ void pluginHandleMenuCommand(CBTYPE cbType, PLUG_CB_MENUENTRY * info)
 
 bool executeScriptCommand(const char* text)
 {
-    //dprintf("Executing %s\n", text);
     py::exec(text);
     return true;
 }
 py::scoped_interpreter* pGuard = nullptr;
 
-FILE* f;
+PYBIND11_EMBEDDED_MODULE(ui, m) {
+    // `m` is a `py::module_` which is used to bind functions and classes
+    m.def("log", _plugin_logprint);
+}
 
-//Initialize your plugin data here.
+// Initialize your plugin data here.
 bool pluginInit(PLUG_INITSTRUCT* initStruct)
 {
     dprintf("Loading...\n");
-
-    AllocConsole();
-    freopen_s(&f, "CONOUT$", "w", stdout);
-    printf("test\n");
-    
-
-
+	
     // start the interpreter and keep it alive
     pGuard = new py::scoped_interpreter{};
     try
     {
-        py::print("Hello, World from python3!\n"); 
+    	// redirect pythons stdout and stderr to x64dbg logfile
+        const auto sys = py::module_::import("sys");
+        const auto ui = py::module_::import("ui");
+        auto py_stdout = sys.attr("stdout");
+        auto py_stderr = sys.attr("stderr");
+
+    	if(py_stdout.is_none())
+    	{
+			// https://github.com/pybind/pybind11/issues/1622
+            // https://pybind11.readthedocs.io/en/stable/advanced/pycpp/utilities.html
+            const auto StringIO = py::module::import("io").attr("StringIO");
+            py_stdout = StringIO(); // Other filelike object can be used here as well, such as objects created by pybind11
+            py_stderr = StringIO();
+            sys.attr("stdout") = py_stdout;
+            sys.attr("stderr") = py_stderr;
+    	}
+        py_stdout.attr("write") = ui.attr("log");
+        py_stderr.attr("write") = ui.attr("log");
+
+
+    	// auto load scriptapi module
+        // const auto scriptapi = py::module_::import("scriptapi");
+        executeScriptCommand("import scriptapi");    	
     }
     catch (std::exception& ex)
     {
         dprintf("Error: %s\n", ex.what());
-        return false;
+        return false; // Return false to cancel loading the plugin.
     }
 
-    // 
-// Register python3 command handler
+	// Register python3 command handler
     SCRIPTTYPEINFO info;
     strcpy_s(info.name, "Python3");
     info.id = 0;
@@ -64,23 +81,19 @@ bool pluginInit(PLUG_INITSTRUCT* initStruct)
     info.completeCommand = nullptr;
     GuiRegisterScriptLanguage(&info);
 	
-    return true; //Return false to cancel loading the plugin.
+    return true;
 }
 
-//Deinitialize your plugin data here.
+// Deinitialize your plugin data here.
 void pluginStop()
 {
+    dprintf("Unloading...\n");
     delete pGuard;
     pGuard = nullptr;
-    dprintf("Unloading...\n");
-    fclose(f);
-    FreeConsole();
 }
 
-//Do GUI/Menu related things here.
+// Do GUI/Menu related things here.
 void pluginSetup()
 {
 	_plugin_menuaddentry(h_menu, menu_entry::MENU_ABOUT, "&About");
 }
-
-
